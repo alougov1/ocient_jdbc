33eb4e4d300993a6d23891c3bed56a35df3f4cc0
jlchen0 <jlchen0@ucla.edu>
2021-04-22 20:01:46 +0000

    [DB-16139] Add getters/setters to GIS types

--- src/main/java/com/ocient/jdbc/StLinestring.java
+++ src/main/java/com/ocient/jdbc/StLinestring.java
@@ -1,7 +1,6 @@
 package com.ocient.jdbc;
 
 import java.util.List;
-
 public class StLinestring
 {
 	private final List<StPoint> points;
@@ -35,43 +34,4 @@ public class StLinestring
         str.append(")");
         return str.toString();
 	}
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-
-        if (o == null || getClass() != o.getClass()) {
-            return false; 
-        }
-
-        StLinestring other = (StLinestring) o;
-
-        // Exact same points 
-        if (other.points.equals(this.points)) {
-            return true; 
-        }
-
-        // Same points, but in reverse order 
-        if (other.points.size() == this.points.size()) {
-            int numPoints = this.points.size(); 
-            for (int i = 0; i < numPoints; ++i) {
-                StPoint mPoint = this.points.get(i); 
-                StPoint oPoint = other.points.get(numPoints - 1 - i); 
-                if (!mPoint.equals(oPoint)) {
-                    return false; 
-                }
-            }
-            return true; 
-        }
-        return false;
-    }
-
-	@Override
-	public int hashCode(){
-        int hash = 0;
-        for (StPoint p : points) {
-            hash += p.hashCode(); 
-        }
-        return hash; 
-	}}
+}
--- src/main/java/com/ocient/jdbc/StPoint.java
+++ src/main/java/com/ocient/jdbc/StPoint.java
@@ -1,23 +1,13 @@
 package com.ocient.jdbc;
-
-import java.util.Objects; 
 public class StPoint
 {
 	private final double lon;
 	private final double lat;
 
-	// For normalizing, necessary to keep member variables final 
-	private double tempLon; 
-	private double tempLat; 
-
 	public StPoint(final double lon, final double lat)
 	{
-		tempLon = lon;
-		tempLat = lat; 
-		normalize(); 
-		
-		this.lon = tempLon;
-		this.lat = tempLat;
+		this.lon = lon;
+		this.lat = lat;
 	}
 
 	public double getLatitude()
@@ -48,59 +38,5 @@ public class StPoint
 			return "POINT EMPTY";
 		}
 		return "POINT(" + lon + " " + lat + ")";
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true; 
-		}
-		
-		if (o == null || getClass() != o.getClass()) {
-			return false; 
-		}
-
-		StPoint other = (StPoint) o;
-		return (this.lon == other.lon) && (this.lat == other.lat); 
-	}
-
-	private void normalize(){ 
-		// See stPoint.h::makeSelfValidAndNormal() 
-		while (tempLon < -180) {
-			tempLon += 360; 
-		}
-
-		while (tempLon >= 180) {
-			tempLon -= 360; 
-		}
-
-		if (tempLat < -90) {
-			tempLat = -180 - lat;
-			tempLon += 180; 
-			normalize(); 
-			return; 
-		}
-
-		if (tempLat > 90) {
-			tempLat = 180 - tempLat;
-			tempLon += 180;
-			normalize(); 
-			return; 
-		}
-
-		double tolerance = 1e-6; 
-		if (Math.abs(90 - Math.abs(tempLat)) < tolerance) {
-			tempLon = 0;
-			if (tempLat > 0) {
-				tempLat = 90;
-			} else {
-				tempLat = -90; 
-			}
-		}
-	}
-	
-	@Override
-	public int hashCode(){
-		return Objects.hash(lon, lat); 
 	}
 }
