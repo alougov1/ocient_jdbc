34dfb9133191d16bc360f702f3a6bab64280f68e
tli-ocient <tli@ocient.com>
2021-12-10 05:18:22 +0000

    Revert "implement state machine parsing"
    This reverts commit 20920feeca2412a1ca12ea6b53b7a9f7a0ebc937.

--- src/main/java/com/ocient/cli/CLI.java
+++ src/main/java/com/ocient/cli/CLI.java
@@ -69,7 +69,6 @@ public class CLI
 
 	private static char quote = '\0';
 	private static boolean comment = false;
-	private static boolean inActiveEscape = false;
 	private static boolean lastCommandErrored = false;
 	private static final char[] hexArray = "0123456789abcdef".toCharArray();
 	private static Statement stmt;
@@ -1340,7 +1339,6 @@ public class CLI
 			final char c = cmd.charAt(i);
 			if (!comment)
 			{
-				// Check for the start of comments
 				if (quote == '\0' && i + 1 != length)
 				{
 					if (c == '-' && cmd.charAt(i + 1) == '-')
@@ -1354,14 +1352,10 @@ public class CLI
 						continue;
 					}
 				}
-				// Check for the start of a quote or the ending of a current quote.
-				if ((c == '\'' || c == '"') && (quote == '\0' || quote == c) && !inActiveEscape) // char is an active quote
+				if ((c == '\'' || c == '"') && (quote == '\0' || quote == c)) // char is an active quote
 				{
 					quote = quote == '\0' ? c : '\0';
 				}
-				// If the current character is a backslash and not already in an active escape,
-				// then we are entering an active escape. Otherwise, we leave activeEscape.
-				inActiveEscape = c == '\\' && !inActiveEscape ? true : false;
 				out.append(c);
 			}
 			else if (i + 1 != length && c == '*' && cmd.charAt(i + 1) == '/')
--- src/main/java/com/ocient/cli/extract/ExtractSyntaxParser.java
+++ src/main/java/com/ocient/cli/extract/ExtractSyntaxParser.java
@@ -3,7 +3,6 @@ package com.ocient.cli.extract;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,6 +22,9 @@ public class ExtractSyntaxParser
     private static String QUOTED_GROUP = "quoted";
     private static String UNQUOTED_GROUP = "unquoted";
 
+    // Courtesy of https://stackoverflow.com/questions/1757065/java-splitting-a-comma-separated-string-but-ignoring-commas-in-quotes?rq=1
+    private static String TOKEN_SPLITTER_STRING = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
+
     private static final Pattern EXTRACT_PATTERN = Pattern.compile(
         "^EXTRACT\\s+TO\\s+(?<" + TYPE_GROUP +
         ">\\w+)\\s+(?:OPTIONS\\((?<" + OPTIONS_GROUP +
@@ -31,24 +33,14 @@ public class ExtractSyntaxParser
         Pattern.CASE_INSENSITIVE);
 
     private static final Pattern ARG_VAL_PATTERN = Pattern.compile(
-        "\\s*(?<" + ARG_GROUP +
+        "(?<" + ARG_GROUP +
         ">\\w+)\\s*=\\s*(?<" + VAL_GROUP +
         ">(?<" + UNQUOTED_GROUP +
         ">\\w+)|(\\\"(?<" + QUOTED_GROUP +
-        ">.*)\\\"))\\s*",
+        ">[^\\\"]*)\\\"))",
         Pattern.CASE_INSENSITIVE
     );
-
-    private enum ParseState
-    {
-        UNQUOTED,
-        QUOTED,
-        QUOTED_ESCAPED
-    }
-
-    private final static char QUOTE_CHARACTER = '\"';
-    private final static char COMMA_CHARACTER = ',';
-    private final static char ESCAPE_CHARACTER = '\\';
+    
     /**
      * A class to wrap the result of an "EXTRACT TO" parse
      */
@@ -117,11 +109,12 @@ public class ExtractSyntaxParser
         }
         
         Map<String, String> keyValuePairs = new HashMap<String,String>();
-        ArrayList<String> tokens = stringTokenizer(ops);
+
+        String[] tokens = ops.split(TOKEN_SPLITTER_STRING, -1);
 
         for(String token : tokens) {
             final Matcher tokenMatcher = ARG_VAL_PATTERN.matcher(token);
-            if(!tokenMatcher.matches()){
+            if(!tokenMatcher.find()){
                 throw new ParseException(String.format("Parsing found bad token: %s", token));
             }
 
@@ -135,73 +128,5 @@ public class ExtractSyntaxParser
         Properties ret = new Properties();
         ret.putAll(keyValuePairs);
         return ret;
-    }
-
-    /**
-     * Seperates key and value(possible quoted) pairs into sets
-     * Ex: key1=val1,key2="val2",key3=val3 -> [key1=val1, key2=val2, key3=val3]
-     * @param options option string containing all the options.
-     * @return ArrayList<String> seperated key=value pairs.
-     * @throws ParseException
-     */
-
-    private static ArrayList<String> stringTokenizer(String options) throws ParseException{
-        
-        ArrayList<String> tokens = new ArrayList<String>();
-        final StringBuilder runningString = new StringBuilder(options.length());
-        char[] chars = options.toCharArray();
-        ParseState currentState = ParseState.UNQUOTED;
-
-        for(char currentChar: chars){
-            switch(currentState){
-                case UNQUOTED:{
-                    if(currentChar == QUOTE_CHARACTER){
-                        // We enter quoted mode
-                        runningString.append(currentChar);
-                        currentState = ParseState.QUOTED;
-                    } else if (currentChar == COMMA_CHARACTER){
-                        if(runningString.length() > 0){
-                            // We finished a string. Add it then clear the curentString
-                            tokens.add(runningString.toString());
-                            runningString.setLength(0);
-                        }
-                    } else {
-                        // Otherwise, just add it to the current string
-                        runningString.append(currentChar);
-                    }
-                    break;
-                }
-                case QUOTED:{
-                    if(currentChar == ESCAPE_CHARACTER){
-                        // We've been escaped. Don't add this character, but enter quote escape
-                        currentState = ParseState.QUOTED_ESCAPED;
-                    } else if(currentChar == QUOTE_CHARACTER){
-                        // We hit the end of our quote
-                        runningString.append(currentChar);
-                        currentState = ParseState.UNQUOTED;
-                    } else {
-                        // Otherwise, just add it to the current string
-                        runningString.append(currentChar);
-                    }
-                    break;
-                }
-                case QUOTED_ESCAPED:{
-                    // Just add the next character, then return back to the quoted state.
-                    runningString.append(currentChar);
-                    currentState = ParseState.QUOTED;
-                    break;
-                }
-            }
-        }
-        // Add the last pair which is not followed by a COMMA_CHARACTER.
-        if(runningString.length() > 0){
-            tokens.add(runningString.toString());
-        }
-        // We should have returned back to the unquoted state at this point.
-        if(currentState != ParseState.UNQUOTED){
-            throw new ParseException(String.format("Malformed options missing a closing quote character: %s", options));
-        }
-
-        return tokens;
     }
 }                        
